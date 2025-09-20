de904b488769d597e0790784b7a8ba5aa3dc88fb
zxcKostya <kostya.kozyulkin.07@bk.ru>
2025-09-20 22:34:14 +0700

    Напишите программу, выводящую количество дней в месяце по заданному номеру месяца и году.
    Формат ввода
    На вход программе подается два целых положительных числа: номер месяца (от 1 до 12) и четырёхзначный год.
    
    Формат вывода
    Необходимо вывести одно число — количество дней в заданном месяце.
    
    Signed-off-by: zxcKostya <kostya.kozyulkin.07@bk.ru>

---
+++ task_15.py
@@ -0,0 +1,28 @@
+month=int(input())
+year=int(input())
+if month==1:
+    print("31")
+elif month==2 and year%4==0:
+    print("29")
+elif month==2:
+    print("28")
+elif month==3:
+    print("31")
+elif month==4 :
+    print("30")
+elif month==5:
+    print("31")
+elif month==6:
+    print("30")
+elif month==7:
+    print("31")
+elif month==8:
+    print("31")
+elif month==9:
+    print("30")
+elif month==10:
+    print("31")
+elif month==11:
+    print("30")
+elif month==12:
+    print("31")
