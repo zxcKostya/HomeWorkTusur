bd06df51a3fcf5b245e8c27552d3b44e6d7a9697
zxcKostya <kostya.kozyulkin.07@bk.ru>
2025-09-27 23:51:04 +0700

    morse
    Signed-off-by: zxcKostya <kostya.kozyulkin.07@bk.ru>

---
+++ task2_2.py
@@ -0,0 +1,9 @@
+x=int(input())
+sam=1
+i=x
+if x>=0:
+    for i in (sam<=x, x-1):
+        sam*=x
+    print(sam)
+else:
+_
---
+++ vv.c++
@@ -0,0 +1,7 @@
+#include <iosream>
+using namespace std:
+int main(){
+ cout<<"hello";
+ return 0;
+}
+
